4c76d89bb2508337f6d014b9fc04e8152925d70b
ashishgole30 <ashish.gole3010@gmail.com>
2026-03-09 16:34:11 +0530

    m

--- DAY 01/ArrayIntroduction.java
+++ DAY 01/ArrayIntroduction.java
@@ -10,7 +10,26 @@ public class ArrayIntroduction {
          * Memory:
          * Index: 0 1 2 3
          * Value: 10 20 30 40
+         * 
+         */
+
+        /*
+         * leetcode 1929
+         * // Array declaration
+         * int[] nums;
+         * // Array length property
+         * int n = nums.length;
+         * // Array creation using new keyword
+         * int[] ans = new int[n * 2];
+         * // For loop for array traversal
+         * for (int i = 0; i < nums.length; i++)
+         * // Array indexing
+         * nums[i]
+         * // Index manipulation
+         * ans[] = nums[i];
+         * ans[i + n] = nums[i];
+         * // Return statement
+         * return ans;
          */
     }
-
 }
